da4a2b40ee7477ab7497f2aa7b658afc8aa8ebff
onehandedpirate <plagueismkii@gmail.com>
2023-05-18 20:49:52 +1000

    Changed default DB to sqlite3, Postgres is optional.

--- URLShortener/settings.py
+++ URLShortener/settings.py
@@ -55,14 +55,19 @@ TEMPLATES = [
 WSGI_APPLICATION = 'URLShortener.wsgi.application'
 
 DATABASES = {
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.postgresql',
+    #     'NAME': env.POSTGRES_DB,
+    #     'USER': env.POSTGRES_USER,
+    #     'PASSWORD': env.POSTGRES_PASSWORD,
+    #     'HOST': env.POSTGRES_HOST,
+    #     'PORT': env.POSTGRES_PORT,
+    # },
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': env.POSTGRES_DB,
-        'USER': env.POSTGRES_USER,
-        'PASSWORD': env.POSTGRES_PASSWORD,
-        'HOST': env.POSTGRES_HOST,
-        'PORT': env.POSTGRES_PORT,
-    }
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    },
+
 }
 
 AUTH_PASSWORD_VALIDATORS = [
