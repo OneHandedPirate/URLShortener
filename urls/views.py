c8539a755809f4d40bdfbbe7598c2a53963e990c
onehandedpirate <plagueismkii@gmail.com>
2023-05-21 16:13:26 +1000

    Minor changes

--- urls/views.py
+++ urls/views.py
@@ -53,7 +53,7 @@ class TokenListView(ListView):
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        context['absolute_url'] = ''.join(self.request.build_absolute_uri().split('/')[:-2])
+        context['absolute_url'] = ''.join(self.request.build_absolute_uri()[:-6])
         context['menu'] = menu
         return context
 
